dacda04b7b7bf370d7a91b9d917980187296f13c
Tendai Chikwiri <tfchikwiri@gmail.com>
2021-10-08 14:44:05 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const menu = () => {
+  let x = document.getElementById("links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
